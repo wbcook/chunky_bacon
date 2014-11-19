bae5fe97398521cb6ca2985a95b53baf64a81366
Wilson <wbcook@users.noreply.github.com>
2014-11-18 20:10:21 -0500

    Create moduleWatchfulSaintAgnes.rb

---
+++ moduleWatchfulSaintAgnes.rb
@@ -0,0 +1,17 @@
+module WatchfulSaintAgnes
+
+	TOOTHLESS_MAN_WITH_FORK = ['man', 'fork', 'exposed gums']
+
+	class FatWaxyChild; end
+
+	def timid_foxfaced_girl; {'please' => 'i want an acorn please'}; end
+
+end
+
+puts WatchfulSaintAgnes::TOOTHLESS_MAN_WITH_FORK
+puts WatchfulSaintAgnes::FatWaxyChild.new
+puts WatchfulSaintAgnes::instance_methods
+
+class TheTimeWarnerAolCitibankCaringAndLovingFacility; end
+TheTimeWarnerAolCitibankCaringAndLovingFacility.extend WatchfulSaintAgnes
+puts TheTimeWarnerAolCitibankCaringAndLovingFacility::timid_foxfaced_girl
