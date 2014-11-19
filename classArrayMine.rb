b59cba5a1b127a37c52bc050b344e2f3e9a6ceb5
Wilson <wbcook@users.noreply.github.com>
2014-11-18 20:09:49 -0500

    Create classArrayMine.rb

---
+++ classArrayMine.rb
@@ -0,0 +1,13 @@
+class ArrayMine < Array
+	# Build a string from this array, formatting each entry
+	# then joining them together.
+	def join( sep = $,, format = "%s" )
+		collect do |item|
+			sprintf( format, item )
+		end.join( sep )
+	end
+end
+
+puts ArrayMine.superclass
+rooms = ArrayMine[3, 4, 6]
+puts "We have " + rooms.join( ", ", "%d bed" ) + " rooms available."
