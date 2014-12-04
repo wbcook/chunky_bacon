5f2422fa21932c88cf9202c1897885f817a92f48
William Cook <wbcook@users.noreply.github.com>
2014-12-04 00:27:17 -0500

    Update dwemthy.rb

--- dwemthy.rb
+++ dwemthy.rb
@@ -33,8 +33,57 @@ class Creature
 
   end
 
+  # This method applies a hit taken during a fight.
+  def hit( damage )
+    p_up = rand( charisma )
+    if p_up % 9 == 7
+      @life += p_up / 4
+      puts "[#{ self.class } magick powers up #{ p_up }!]"
+    end
+    @life -= damage
+    puts "[#{ self.class } has died.]" if @life <= 0
+  end
+
+  # This method takes on turn in a fight.
+  def fight( enemy, weapon )
+    if life <= 0
+      puts "[#{ self.class } is too dead to fight!]"
+      return
+    end
+    
+    # Attack the opponent
+    your_hit = rand( strength + weapon )
+    puts "[You hit with #{ your_hit } points of damage!]"
+    enemy.hit( your_hit )
+
+    # Retaliation
+    puts "[#{ enemy.class } has #{enemy.life} life remaining!!]"
+    if enemy.life > 0
+      enemy_hit = rand( enemy.strength + enemy.weapon )
+      puts "[Your enemy hit with #{ enemy_hit } points of damage!]"
+      self.hit( enemy_hit )
+    end
+  end
+
   # Creature attributes are read-only
   traits :life, :strength, :charisma, :weapon
+end
+
+class DwemthysArray < Array
+  alias _inspect inspect
+  def inspect; "#<#{ self.class }#{ _inspect }>"; end
+  def method_missing( meth, *args )
+    answer = first.send( meth, *args )
+    if first.life <= 0
+    shift
+      if empty?
+        puts "[Whoa. You decimated Dwemthy's Array!]"
+      else
+        puts "[Get ready. #{ first.class } has emerged.]"
+      end
+    end
+    answer || 0
+  end
 end
 
 class Dragon < Creature
@@ -44,6 +93,61 @@ class Dragon < Creature
   weapon 939    # fire breath
 end
 
+class ScubaArgentine < Creature
+  life 46
+  strength 35
+  charisma 91
+  weapon 2
+end
+# Rabbit should be its own .rb file. Page 115 using the irb and require.
+class Rabbit < Creature
+  traits :bombs
+
+  life 10
+  strength 2
+  charisma 44
+  weapon 4
+  bombs 3
+
+  # little boomerang
+  def ^( enemy )
+    fight( enemy, 13 )
+  end
+  # the hero's sword is unlimited!!
+  def /( enemy )
+    fight( enemy, rand( 4 + ( ( enemy.life % 10 ) ** 2 ) ) )
+  end
+  # lettuce will build your strength and extra ruffage
+  # will fly in the face of your opponent!!
+  def %( enemy )
+    lettuce = rand( charisma )
+    puts "[Healthy lettuce gives you #{ lettuce } life points!!]"
+    @life += lettuce
+    fight( enemy, 0 )
+  end
+  # bombs, but you only have three!!
+  def *( enemy )
+    if @bombs.zero?
+      puts "[UHN!! You're out of bombs!!]"
+      return
+    end
+    @bombs -= 1
+    fight( enemy, 86 )
+  end
+end
+
+r = Rabbit.new
+s = ScubaArgentine.new
+
+r ^ s
+
+#r / 2
+
+r % s
+
+r * s
+
+=begin
 print "What monster class have you come to battle?"
 monster, drgn = gets.chomp
 eval( "drgn = #{monster}.new" )
@@ -52,3 +156,4 @@ print "What is #{monster}'s name?"
 monster_name = gets.chomp
 monster.instance_eval { @name= monster_name }
 p monster.instance_variable_get( "@name" )
+=end
